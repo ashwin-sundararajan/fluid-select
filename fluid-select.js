d8bcd7a7d65d232a788e6f2ff982d838c95c23a9
Ashwin Sundararajan <ashwin.sundararajan@freshworks.com>
2019-07-24 15:51:28 +0530

    basic html generation

--- fluid-select.js
+++ fluid-select.js
@@ -1,2 +1,66 @@
 "use strict";
-console.log('hello there');
+var FluidSelect = /** @class */ (function () {
+    function FluidSelect(initValues) {
+        var _a, _b, _c;
+        var _this = this;
+        this.values = {};
+        this.selectedValue = null;
+        initValues.forEach(function (option) {
+            _this.values[option.value] = option;
+        });
+        this.element = FluidSelect.createElement('div', (_a = {},
+            _a['class'] = 'select__container',
+            _a));
+        this.displayContainer = FluidSelect.createElement('div', (_b = {},
+            _b['class'] = 'select__display',
+            _b));
+        this.displayContainer.textContent = 'Choose a value';
+        this.element.appendChild(this.displayContainer);
+        this.optionContainer = FluidSelect.createElement('div');
+        this.element.appendChild(this.optionContainer);
+        this.searchInput = FluidSelect.createElement('input', (_c = {},
+            _c['class'] = 'select__input',
+            _c));
+        this.optionContainer.appendChild(this.searchInput);
+        var values = this.values;
+        var _loop_1 = function (value) {
+            var _a;
+            var newDiv = FluidSelect.createElement('div', (_a = {},
+                _a['class'] = 'select__option',
+                _a), {
+                click: function () {
+                    if (_this.selectedValue) {
+                        values[_this.selectedValue.value].selected = false;
+                    }
+                    values[value].selected = true;
+                    _this.setValue(value);
+                }
+            });
+            newDiv.textContent = values[value].label;
+            this_1.optionContainer.appendChild(newDiv);
+        };
+        var this_1 = this;
+        for (var value in values) {
+            _loop_1(value);
+        }
+    }
+    FluidSelect.createElement = function (type, attributes, eventListeners) {
+        var element = document.createElement(type);
+        if (attributes) {
+            for (var key in attributes) {
+                element.setAttribute(key, attributes[key]);
+            }
+        }
+        return element;
+    };
+    FluidSelect.prototype.showOptions = function () {
+        this.optionContainer.classList.remove('hide');
+    };
+    FluidSelect.prototype.hideOptions = function () {
+        this.optionContainer.classList.add('hide');
+    };
+    FluidSelect.prototype.setValue = function (value) {
+        this.selectedValue = this.values[value];
+    };
+    return FluidSelect;
+}());
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,108 @@
-console.log('hello there');
+interface Option {
+  value: string;
+  label: string;
+  selected?: boolean;
+}
+
+interface SelectOptions {
+  [index: string]: Option;
+}
+
+interface AttributeDescriptor {
+  [index: string]: string;
+}
+
+interface EventDescriptor {
+  [index: string]: (event: Event) => void;
+}
+
+class FluidSelect {
+  values: SelectOptions = {};
+  element: HTMLDivElement;
+  displayContainer: HTMLDivElement;
+  searchInput: HTMLInputElement;
+  private optionContainer: HTMLDivElement;
+  selectedValue: Option | null = null;
+
+  constructor(initValues: Option[]) {
+    initValues.forEach(
+      (option: Option): void => {
+        this.values[option.value] = option;
+      }
+    );
+
+    this.element = FluidSelect.createElement('div', {
+      ['class']: 'select__container'
+    }) as HTMLDivElement;
+
+    this.displayContainer = FluidSelect.createElement('div', {
+      ['class']: 'select__display'
+    }) as HTMLDivElement;
+
+    this.displayContainer.textContent = 'Choose a value';
+
+    this.element.appendChild(this.displayContainer);
+
+    this.optionContainer = FluidSelect.createElement('div') as HTMLDivElement;
+    this.element.appendChild(this.optionContainer);
+
+    this.searchInput = FluidSelect.createElement('input', {
+      ['class']: 'select__input'
+    }) as HTMLInputElement;
+
+    this.optionContainer.appendChild(this.searchInput);
+
+    const { values } = this;
+
+    for (let value in values) {
+      const newDiv = FluidSelect.createElement(
+        'div',
+        {
+          ['class']: 'select__option'
+        },
+        {
+          click: () => {
+            if (this.selectedValue) {
+              values[this.selectedValue.value].selected = false;
+            }
+
+            values[value].selected = true;
+            this.setValue(value);
+          }
+        }
+      );
+
+      newDiv.textContent = values[value].label;
+
+      this.optionContainer.appendChild(newDiv);
+    }
+  }
+
+  static createElement(
+    type: string,
+    attributes?: AttributeDescriptor,
+    eventListeners?: EventDescriptor
+  ): HTMLElement {
+    const element = document.createElement(type);
+
+    if (attributes) {
+      for (let key in attributes) {
+        element.setAttribute(key, attributes[key]);
+      }
+    }
+
+    return element;
+  }
+
+  showOptions() {
+    this.optionContainer.classList.remove('hide');
+  }
+
+  hideOptions() {
+    this.optionContainer.classList.add('hide');
+  }
+
+  setValue(value: string) {
+    this.selectedValue = this.values[value];
+  }
+}
